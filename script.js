5b4c32d25bad5dc666787356bb4987ecef0d6196
Shuvo <suvo10.info@gmail.com>
2026-01-14 03:01:10 +0600

    yjuu

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const startText = document.getElementById('startText');
 const player1ScoreEl = document.getElementById('player1score');
 const player2ScoreEl = document.getElementById('player2score');
 const paddle1=document.getElementById('paddle1');
+const paddle2 = document.getElementById('paddle2');
 const maxPaddleSpeed=5;
 
 // Game variables
@@ -12,6 +13,8 @@ let player2Score = 0;
 let keysPressed = {};
 let paddle1Speed = 0;
 let paddle1Y = 150;
+let paddle2Speed = 0;
+let paddle2Y = 150;
 
 initScoreboard();
 
@@ -48,6 +51,7 @@ function initScoreboard() {
 function gameLoop() {
        if (gameRunning) {
               updatePaddle1();
+              updatePaddle2();
               setTimeout(gameLoop, 8);
        }
 }
@@ -81,6 +85,25 @@ function updatePaddle1() {
        paddle1.style.top = paddle1Y + 'px';
 }
 
+function updatePaddle2() {
+       if (!paddle2) {
+              return;
+       }
+       if (keysPressed['ArrowUp']) {
+              paddle2Speed = -5;
+       } else if (keysPressed['ArrowDown']) {
+              paddle2Speed = 5;
+       } else {
+              paddle2Speed = 0;
+       }
+       paddle2Y += paddle2Speed;
+       if (paddle2Y < 0) {
+              paddle2Y = 0;
+       } else if (paddle2Y > 300) {
+              paddle2Y = 300;
+       }
+       paddle2.style.top = paddle2Y + 'px';
+}
 
 
 
